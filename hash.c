99673bc85dba2ac74cc370b72f4dce6a90b1720f
pHgon <phenrikgoncalves@gmail.com>
2016-12-14 23:25:23 -0200

    Mudanças na base Linear

--- hash.c
+++ hash.c
@@ -6,34 +6,9 @@
 #define LINEAR       1
 #define QUADRATICA   2
 #define DUPLO        3
+#define L_FACTOR      0.75
+#define INI_SIZE      500
 
-int option (char argument[]);
-
-void main(int argc,char *argv[]){
-    if(argc == 1){
-    	printf("\nERROR: No argument received .. Program closed!\n");
-    	exit(-1);
-    }
-	
-	FILE *file = fopen (argv[2], "r");
-	if (file == NULL){
-		printf("\nERROR: File not found .. Program closed!\n");
-		exit(-1);
-	}
-
-	switch (option(argv[1])){
-		case ENCADEAMENTO:
-			break;
-		case LINEAR:
-			break;
-		case QUADRATICA:
-			break;
-		case DUPLO:
-			break;
-		default:
-			break;
-	}
-}
 
 
 int option (char argument[]){
@@ -53,4 +28,73 @@ int option (char argument[]){
 			}
 		}
 	}
+}
+
+FILE *iniOutputFile (){
+	FILE *file = fopen("log_output.txt", "w");
+	return file;
+}
+
+// Funcao HASH Rotation ou Rolling, recebe a chave a ser codificada e o tamanho
+unsigned rot_hash (void *key, int len){
+    unsigned char *p = key;
+    unsigned h = 0;
+    int i;
+    for (i = 0; i < len; i++) {
+        h = (h << 4) ^ (h >> 28) ^ p[i];
+    }
+    return h;
+}
+
+// Funcao Le do arquivo de entrada
+unsigned readInput (FILE *inputFile, char input1[], char input2[]){
+	fscanf(inputFile, "%s %s", input1, input2);
+	int tam = strlen(input2);
+	tam--;
+	input2[tam] = '\0'; // Limpa as " do final da string
+	return rot_hash(input2, tam);
+}
+
+void linear (FILE *inputFile, FILE *outputFile){
+	char input1[7], input2[100];
+	unsigned key;
+
+	key = readInput(inputFile, input1, input2);
+	
+	//printf("%s | %s | %u | %u\n\n", input1, input2, key, key%500);
+}
+
+
+void main(int argc,char *argv[]){
+    if(argc == 1){
+    	printf("\nERROR: No argument received .. Program closed!\n");
+    	exit(-1);
+    }
+	
+	FILE *inputFile = fopen (argv[2], "r");
+	if (inputFile == NULL){
+		printf("\nERROR: File not found .. Program closed!\n");
+		exit(-1);
+	}
+
+	FILE *outputFile;
+
+	switch (option(argv[1])){
+		case ENCADEAMENTO:
+			break;
+		case LINEAR:
+			outputFile = iniOutputFile();
+			linear(inputFile, outputFile);
+			exit(1);
+			break;
+		case QUADRATICA:
+			break;
+		case DUPLO:
+			break;
+		default:
+			printf("\nERROR: Invalid Argument .. Program closed!\n");
+			break;
+	}
+	fclose(inputFile);
+	fclose(outputFile);
 }

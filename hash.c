931d7e561e4ed0adfa113b982c979ea1287dd5b0
pHgon <phenrikgoncalves@gmail.com>
2017-02-21 00:15:45 -0300

    Migrado para Structs

--- hash.c
+++ hash.c
@@ -9,6 +9,12 @@
 #define L_FACTOR     0.75
 #define INI_SIZE     500
 
+// Celula Hash
+struct Node{
+	char keyString[101];
+	struct Node *prox;
+}; typedef struct Node celHash;
+
 
 int option (char argument[]){
 	if ((strcmp(argument, "-encadeamento")) == 0)
@@ -30,31 +36,29 @@ int option (char argument[]){
 }
 
 // Inicia a Hash
-char **startHash (int size){
+celHash **startHash (int size){
 	int i;
-	char **ptr;
-	ptr = (char **) malloc (size * sizeof(char *));
+	celHash **ptr;
+	ptr = (celHash **) malloc (size * sizeof(celHash *));
 	if(ptr==NULL){
 		printf("ERROR: Null pointer!\n");
 		exit(0);
 	}
-	for (i=0; i<size; i++){
-		ptr[i] = (char *) malloc (100 * sizeof(char));
-		if(ptr[i]==NULL){
-			printf("ERROR: Null pointer!\n");
-			exit(0);
-		}
-	}
 	return ptr;
 }
 
-void destroyHash(char **ptr, int size){
+// Libera a Hash
+void destroyHash(celHash **ptr, int size){
 	int i;
 	for (i=0; i<size; i++){
 		free(ptr[i]);
 	}
 	free(ptr);
 }
+
+//char **rehash (char **ptr, &size){
+//
+//}
 
 // Funcao HASH Rotation ou Rolling, recebe a chave a ser codificada e o tamanho
 int rot_hash(void *key, int len){
@@ -66,6 +70,10 @@ int rot_hash(void *key, int len){
     }
     return abs(h);
 }
+
+// Funcoes de dispersao hash primaria e secundaria
+int f_hash(int key, int size){	return key%size;  }
+int s_hash(int key, int size){	return 1+f_hash(key, size-1);  }
 
 // Le do arquivo de entrada e retorna o valor da Key do Rotation Hash
 int readInput (FILE *inputFile, char *input1, char *input2){
@@ -80,12 +88,13 @@ int readInput (FILE *inputFile, char *input1, char *input2){
 }
 
 // Insere na Hash
-void insert (char **ptr, int size, char *input, int key){
+void insert (celHash **ptr, int size, char *input, int key){
 	int i = key;
 	do{
-		if (ptr[i][0] == '\0'){
-			strncpy(ptr[i], input, 100);
-			printf("%d - %s\n", i, input);
+		if (ptr[i] == NULL){
+			ptr[i] = (celHash *) malloc (sizeof(celHash));
+			strncpy(ptr[i]->keyString, input, 101);
+			printf("%d - %s\n", i, ptr[i]->keyString);
 			return;
 		}
 		else{	
@@ -99,18 +108,19 @@ void insert (char **ptr, int size, char *input, int key){
 void linear (FILE *inputFile, FILE *outputFile){
 	char input1[7], input2[100];
 	int key, hashSize = INI_SIZE, loadHash = 0;
-	char **head = startHash(hashSize);
+	celHash **head = startHash(hashSize);
 	
 	while (!feof(inputFile)){
 		key = readInput(inputFile, input1, input2);
 
 		if (key > 0){
-			key = key%hashSize;
+			key = f_hash(key, hashSize);
 
 			if(strcmp(input1, "INSERT") == 0){
 				insert(head, hashSize, input2, key);
 				loadHash++;
 				//if((loadHash/hashSize)>=L_FACTOR)
+				//	head = rehash(head, &hashSize);
 			}
 			else{
 				if(strcmp(input1, "DELETE") == 0){
@@ -128,11 +138,8 @@ void linear (FILE *inputFile, FILE *outputFile){
 			}
 		}
 	}
-
 	destroyHash(head, hashSize);
 }
-
-
 
 
 void main(int argc,char *argv[]){
